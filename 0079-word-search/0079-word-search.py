3a7ff0808a2e40fc222dc1fba9c213c86da0c5ae
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-04 22:27:21 +0530

    Time: 3724 ms (55.61%), Space: 19.4 MB (57.3%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,23 +3,17 @@ class Solution:
         directions = [
             (1, 0), (-1, 0), (0, 1), (0, -1)
         ]
-
         rows = len(board)
         cols = len(board[0])
-
         def dfs(r, c, i):
             if r < 0 or c < 0 or r >= rows or c >= cols:
                 return False
-
             if board[r][c] != word[i]:
                 return False
-
             if i == len(word) - 1:
                 return True
-
             temp = board[r][c]
             board[r][c] = "#"
-
             for x, y in directions:
                 nr = r + x
                 nc = c + y
@@ -27,10 +21,8 @@ class Solution:
                 if dfs(nr, nc, i + 1):
                     board[r][c] = temp
                     return True
-
             board[r][c] = temp
             return False
-
         for i in range(rows):
             for j in range(cols):
                 if dfs(i, j, 0):
