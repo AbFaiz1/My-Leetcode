f3944d374ee0ccd78a9fe2728f4c8676abcc16ff
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-10 12:01:03 +0530

    Time: 2630 ms (5.25%), Space: 33.7 MB (35.13%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,39 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+
+        parent = list(range(n))
+
+        def find(x):
+            if parent[x] == x:
+                return x
+            return find(parent[x])
+
+        def union(x, y):
+            px = find(x)
+            py = find(y)
+
+            if px != py:
+                parent[px] = py
+
+        count = 0
+
+        # Minimum n - 1 cables are required
+        if len(connections) < n - 1:
+            return -1
+
+        for u, v in connections:
+            if find(u) == find(v):
+                count += 1
+            else:
+                union(u, v)
+
+        components = 0
+
+        for i in range(n):
+            if find(i) == i:
+                components += 1
+
+        if count >= components - 1:
+            return components - 1
+
+        return -1
