c1e58d115868f79194237252edca934b172a0c0c
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-11 17:02:46 +0530

    Time: 251 ms (54.38%), Space: 22.6 MB (54.88%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -5,28 +5,32 @@ class Solution:
         directions = [(1,0), (-1,0), (0,1), (0,-1)]
         rows = len(grid)
         cols = len(grid[0])
-
+        
         queue = deque()
-        queue.append((0, 0, 0, 0))
-
-        visited = [[float('inf')] * cols for _ in range(rows)]
-        visited[0][0] = 0
-
-        while queue:
-            row, col, dis, ok = queue.popleft()
-
-            if row == rows - 1 and col == cols - 1:
-                return dis
-
-            for x, y in directions:
-                nr = row + x
-                nc = col + y
-
-                if 0 <= nr < rows and 0 <= nc < cols:
-                    new_count = ok + grid[nr][nc]
-
-                    if new_count <= k and new_count < visited[nr][nc]:
-                        visited[nr][nc] = new_count
-                        queue.append((nr, nc, dis + 1, new_count))
-
-        return -1
+        
+        def bfs(r, c, dis, count):
+            queue.append((r, c, dis, count))
+            
+            visited = [[set() for _ in range(cols)] for _ in range(rows)]
+            visited[r][c].add(count)
+            
+            while queue:
+                row, col, dis, ok = queue.popleft()
+                
+                if row == rows - 1 and col == cols - 1:
+                    return dis
+                
+                for x, y in directions:
+                    nr = row + x
+                    nc = col + y
+                    
+                    if 0 <= nr < rows and 0 <= nc < cols:
+                        new_count = ok + grid[nr][nc]
+                        
+                        if new_count <= k and new_count not in visited[nr][nc]:
+                            visited[nr][nc].add(new_count)
+                            queue.append((nr, nc, dis + 1, new_count))
+            
+            return -1
+        
+        return bfs(0, 0, 0, 0)
