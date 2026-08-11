1e02f51b16372a5601b4438cdcb256155332399c
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-12 00:41:52 +0530

    Time: 3959 ms (45.96%), Space: 19.4 MB (83.33%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,31 +1,32 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
+    def exist(self, grid: List[List[str]], word: str) -> bool:
         directions = [
             (1, 0), (-1, 0), (0, 1), (0, -1)
         ]
-        rows = len(board)
-        cols = len(board[0])
+        rows = len(grid)
+        cols = len(grid[0])
         def dfs(r, c, i):
-            if r < 0 or c < 0 or r >= rows or c >= cols:
-                return False
-            if board[r][c] != word[i]:
-                return False
-            if i == len(word) - 1:
+            if r >= rows or c >= cols or r < 0 or c < 0:
+                return 
+            if i == len(word):
                 return True
-            temp = board[r][c]
-            board[r][c] = "#"
+            if grid[r][c] != word[i] or grid[r][c] == "#":
+                return                    
+            temp = grid[r][c]
+            grid[r][c] = "#"
             for x, y in directions:
-                nr = r + x
-                nc = c + y
-
-                if dfs(nr, nc, i + 1):
-                    board[r][c] = temp
-                    return True
-            board[r][c] = temp
-            return False
+                nr = x + r
+                nc = y + c
+                if dfs(nr, nc, i+1):
+                    return True 
+            grid[r][c] = temp          
+            return 
+        if len(grid) == 1 and len(grid[0]) == 1 and len(word) == 1:
+            if grid[0][0] == word[0]:
+                return True
         for i in range(rows):
             for j in range(cols):
                 if dfs(i, j, 0):
                     return True
-
         return False
+    
