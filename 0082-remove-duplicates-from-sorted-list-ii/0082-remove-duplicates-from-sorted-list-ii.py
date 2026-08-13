6d42058e2abb8e15bfe35f2b00c51689ce8af18e
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-13 22:25:28 +0530

    Time: 1 ms (38.47%), Space: 19.2 MB (72.72%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        # CHANGE: empty linked list handle karne ke liye
         if not head:
             return head
 
@@ -8,29 +10,30 @@ class Solution:
         prev = head
 
         while temp:
+            # CHANGE: head.val ki jagah temp.val compare hoga
+            # kyunki adjacent nodes ko compare karke duplicate detect karna hai
             if temp.val == prev.val:
                 duplicates.add(temp.val)
+
             temp = temp.next
             prev = prev.next
 
-        if head.val in duplicates:
-            head = head.next
-            while head and head.val in duplicates:
-                head = head.next
+        # CHANGE: dummy node add kiya
+        # taaki agar head khud duplicate ho, to easily remove ho sake
+        dummy = ListNode(0)
+        dummy.next = head
 
-        prev = head
-
-        if not prev:
-            return None
-
-        temp = head.next
+        prev = dummy
+        temp = head
 
         while temp:
             if temp.val not in duplicates:
                 prev.next = temp
                 prev = prev.next
+
             temp = temp.next
 
         prev.next = None
 
-        return head
+ 
+        return dummy.next
