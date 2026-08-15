1677587d6e94f946b13e24a90a90ce8148d53bbb
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-15 17:12:43 +0530

    Time: 0 ms (100%), Space: 19.4 MB (67.67%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,13 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, arr: List[int]) -> List[List[int]]:
         ans = []
         def solve(i, temp):
-            if i >= len(nums):
-                ans.append(temp[:])
-                return 
-            
-            temp.append(nums[i])
+            if i >= len(arr):
+                ans.append(temp.copy())
+                return
+            temp.append(arr[i])
             solve(i+1, temp)
             temp.pop()
             solve(i+1, temp)
         solve(0, [])
         return ans
-            
