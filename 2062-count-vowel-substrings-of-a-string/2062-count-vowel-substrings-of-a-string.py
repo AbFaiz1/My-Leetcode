cf74c1a7dcd6b45845eb96a5a8bf6bfe8fd1f9ab
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-22 21:28:11 +0530

    Time: 0 ms (100%), Space: 19.1 MB (89.43%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.py
@@ -1,33 +1,23 @@
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
-
         vowels = set("aeiou")
-
-        def atMost(k):
-
-            freq = {}
-            left = 0
+        def atmost(k):
             ans = 0
-
-            for right in range(len(word)):
-
-                if word[right] not in vowels:
-                    freq.clear()
-                    left = right + 1
+            mp = {}
+            left = 0
+            for i in range(len(word)):
+                if word[i] not in vowels:
+                    mp.clear()
+                    left = i + 1
                     continue
-
-                freq[word[right]] = freq.get(word[right], 0) + 1
-
-                while len(freq) > k:
-                    freq[word[left]] -= 1
-
-                    if freq[word[left]] == 0:
-                        del freq[word[left]]
-
+                mp[word[i]] = mp.get(word[i], 0) + 1
+                while len(mp) > k:
+                    mp[word[left]] -= 1
+                    if mp[word[left]] == 0:
+                        del mp[word[left]]
                     left += 1
-
-                ans += right - left + 1
-
+                ans += i - left + 1
             return ans
-
-        return atMost(5) - atMost(4)
+        return atmost(5) - atmost(4)
+                
+            
