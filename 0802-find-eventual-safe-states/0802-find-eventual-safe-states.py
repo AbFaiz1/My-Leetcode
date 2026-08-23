5b636156cea96d93026bfa0b7f62ccc60ae7a449
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-23 23:42:13 +0530

    Time: 323 ms (5.1%), Space: 28.7 MB (15.54%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -4,23 +4,17 @@ class Solution:
         visiting = set()
         safe = set()
         def dfs(start):
-
             if start in visiting:
                 return False
-
             if start in safe:
                 return True
-
             visiting.add(start)
-
             for nei in graph[start]:
                 if not dfs(nei):
                     visiting.remove(start)
                     return False
-
             visiting.remove(start)
             safe.add(start)
-
             return True
         ans = []
         for i in range(len(graph)):
