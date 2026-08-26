57bd04aaa4151a3de47cc79540ed873d5efb7d4d
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-26 12:14:45 +0530

    Time: 0 ms (100%), Space: 19.2 MB (94.53%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -4,26 +4,16 @@ class Solution:
         ob = n
         cb = n
         end = n * 2
-
         def solve(i, temp, ob, cb):
-
             if i == end:
                 ans.append(temp)
                 return
-
-            if cb > ob:  # FIX: ob != 0 hatao; ob=0 hone par bhi ')' le sakte hain
-
-                # take: ')' place karo
+            if cb > ob:  
                 solve(i + 1, temp + ")", ob, cb - 1)
-
-                # skip: '(' place karo
                 if ob > 0:  
                     solve(i + 1, temp + "(", ob - 1, cb)
-
             else:
                 if ob > 0:  
                     solve(i + 1, temp + "(", ob - 1, cb)
-
         solve(0, "", n, n)
-
         return ans
