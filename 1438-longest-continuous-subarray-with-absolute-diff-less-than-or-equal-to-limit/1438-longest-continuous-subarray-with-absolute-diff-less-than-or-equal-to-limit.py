76d09428b50f7c22522676f1e5243c5be97f839f
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-29 14:28:17 +0530

    Time: 279 ms (20.69%), Space: 30.3 MB (64.36%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -3,26 +3,26 @@ class Solution:
     def longestSubarray(self, arr: List[int], limit: int) -> int:
         dqmini = deque()
         dqmaxi = deque()
-        def insertmini(idx):
-            while dqmini and arr[idx] < arr[dqmini[-1]]:
+        def insertmini(val):
+            while dqmini and val < dqmini[-1]:
                 dqmini.pop()
-            dqmini.append(idx)  
-        def insertmaxi(idx):
-            while dqmaxi and arr[idx] > arr[dqmaxi[-1]]:
+            dqmini.append(val)
+        def insertmaxi(val):
+            while dqmaxi and val > dqmaxi[-1]:
                 dqmaxi.pop()
-            dqmaxi.append(idx)  
+            dqmaxi.append(val)
         left = 0
         right = 0
         ans = 0
         while right < len(arr):
-            insertmini(right)
-            insertmaxi(right)
-            while (arr[dqmaxi[0]] - arr[dqmini[0]]) > limit:
-                if dqmini[0] == left:  
+            insertmini(arr[right])
+            insertmaxi(arr[right])
+            while left < len(arr) and dqmini and dqmaxi and abs(dqmini[0] - dqmaxi[0]) > limit:
+                if arr[left] == dqmini[0]:
                     dqmini.popleft()
-                if dqmaxi[0] == left:  
+                if arr[left] == dqmaxi[0]:
                     dqmaxi.popleft()
                 left += 1
-            ans = max(ans, right - left + 1)
-            right += 1
+            ans = max(ans, right-left+1)
+            right += 1 
         return ans
