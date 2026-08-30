dee150fb5cd5d1cce9bffa661bf2a443f30d2679
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-08-30 12:59:09 +0530

    Time: 7 ms (57.18%), Space: 21.4 MB (8.01%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,29 @@
+from functools import cache
+
+class Solution:
+
+    def isMatch(self, s: str, p: str) -> bool:
+
+        @cache  
+        def solve(i, j):
+
+            if j == len(p):
+                return i == len(s)
+
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+
+            if j + 1 < len(p) and p[j + 1] == "*":
+
+                if solve(i, j + 2):
+                    return True
+
+                if match and solve(i + 1, j):
+                    return True
+
+            if match:
+                if solve(i + 1, j + 1):
+                    return True
+
+            return False
+
+        return solve(0, 0)
