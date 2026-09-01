ead150f5e9872a30c0377b4e8fa9df362d73c74f
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-01 18:51:13 +0530

    Time: 495 ms (99.78%), Space: 64.9 MB (63.5%) - LeetHub

---
+++ 1882-process-tasks-using-servers/1882-process-tasks-using-servers.py
@@ -0,0 +1,26 @@
+from heapq import heappop,heappush
+
+class Solution:
+    def assignTasks(self,servers:List[int],tasks:List[int])->List[int]:
+        free=[]
+        busy=[]
+        ans=[0]*len(tasks)
+        for i in range(len(servers)):
+            heappush(free,(servers[i],i))
+        for i in range(len(tasks)):
+            while busy and busy[0][0]<i:
+                end,w,idx=heappop(busy)
+                heappush(free,(w,idx))
+
+            if free:
+                w,idx=heappop(free)
+                ans[i]=idx
+                end=i+tasks[i]-1
+                heappush(busy,(end,w,idx))
+            else:
+                end,w,idx=heappop(busy)
+                ans[i]=idx
+                end=end+tasks[i]
+                heappush(busy,(end,w,idx))
+
+        return ans
