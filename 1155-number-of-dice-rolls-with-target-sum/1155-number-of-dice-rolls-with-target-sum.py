d3264054f434cf92aabd7ce21a637047adc4f220
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-01 23:11:16 +0530

    Time: 185 ms (73.52%), Space: 30.7 MB (23.84%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,22 +1,20 @@
+from functools import lru_cache
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        mp = {}
+        MOD = 10 ** 9 + 7
+        @lru_cache(None)
         def solve(temp, dice):
-            if temp > target:
-                return 0
-            if temp == target:
-                if dice  == n:
+            if dice == n:
+                if temp == target:
                     return 1
                 return 0
-            if dice >= n:
+            if temp > target:
                 return 0
-            if (temp, dice) in mp:
-                return mp[(temp, dice)]
             ans = 0
             for i in range(1, k+1):
-                choice = solve(temp + i, dice + 1)
-                ans += choice
-            mp[(temp, dice)] = ans %(10**9 + 7)
-            return mp[(temp, dice)]
-        return solve(0, 0) % (10**9 + 7)
+                c1 = solve(temp + i, dice+1)
+                ans += c1 % MOD
+            return ans % MOD
+        return solve(0, 0)
+
                 
