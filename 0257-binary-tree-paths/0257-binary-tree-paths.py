673472bb01fa9fd7e8a12da103c886113bfb191b
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-04 21:54:31 +0530

    Time: 0 ms (100%), Space: 19.4 MB (61.96%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -1,22 +1,16 @@
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         ans = []
-
-        def dfs(root, s):
+        def dfs(root, temp):
             if root is None:
                 return
-
-            s.append(str(root.val))
-
-            if root.left is None and root.right is None:  
-                ans.append("->".join(s))  
-                s.pop()  
+            temp.append(root.val)
+            if root.left is None and root.right is None:
+                ans.append("->".join(map(str, temp)))
+                temp.pop()
                 return
-
-            dfs(root.left, s)
-            dfs(root.right, s)
-
-            s.pop()  
-
-        dfs(root, [])
+            dfs(root.left, temp)
+            dfs(root.right, temp)
+            temp.pop()
+        dfs(root, []) 
         return ans
