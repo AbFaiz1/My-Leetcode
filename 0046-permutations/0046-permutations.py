286d675ca7c0bea457620ea02c5744d8b8bc3070
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-07 18:59:58 +0530

    Time: 3 ms (31.47%), Space: 19.5 MB (35.73%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,23 +2,17 @@ class Solution:
     def permute(self, arr: List[int]) -> List[List[int]]:
         visited = set()
         ans = []
-
         def solve(temp):
-            if len(temp) == len(arr):  # FIX: complete permutation ban gayi
+            if len(temp) == len(arr):
                 ans.append(temp.copy())
-                return
-
+                return 
             for i in range(len(arr)):
                 if i in visited:
                     continue
-
                 visited.add(i)
                 temp.append(arr[i])
-
-                solve(temp)  
-
-                temp.pop()        # FIX: backtrack
-                visited.remove(i) # FIX: same element ko unvisit karo
-
+                solve(temp)
+                temp.pop()
+                visited.remove(i)
         solve([])
         return ans
