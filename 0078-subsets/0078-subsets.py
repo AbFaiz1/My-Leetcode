23f0046de0fe25ce2a757eaafb35eabb890902fa
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-07 18:34:24 +0530

    Time: 0 ms (100%), Space: 19.4 MB (68.64%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -5,9 +5,9 @@ class Solution:
             if i >= len(arr):
                 ans.append(temp.copy())
                 return
+            solve(i+1, temp)
             temp.append(arr[i])
             solve(i+1, temp)
             temp.pop()
-            solve(i+1, temp)
         solve(0, [])
         return ans
