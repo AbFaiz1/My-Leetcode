48503b2b64b83a3306d170fdaf3d1caf565f9a8b
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-07 22:49:52 +0530

    Time: 123 ms (43.45%), Space: 22.3 MB (13.39%) - LeetHub

--- 0813-largest-sum-of-averages/0813-largest-sum-of-averages.py
+++ 0813-largest-sum-of-averages/0813-largest-sum-of-averages.py
@@ -2,16 +2,18 @@ from functools import lru_cache
 class Solution:
     def largestSumOfAverages(self, arr: List[int], k: int) -> float:
         @lru_cache(None)
-        def solve(i, group):
-            if i == len(arr):
-                return 0
-            if group == k-1:
-                return sum(arr[i:]) / (len(arr) - i)
+        def solve(i, groups):
             total = 0
             ans = 0
+            if groups == k - 1:
+                if i < len(arr):
+                    return sum(arr[i:]) / (len(arr) - i)
+                return float("-inf")
             for j in range(i, len(arr)):
                 total += arr[j]
-                c = (total / (j-i+1)) + solve(j+1, group+1)
-                ans = max(ans, c)
+                c = (total / (j - i + 1)) + solve(j+1, groups + 1)
+                ans = max(c, ans)
             return ans
         return solve(0, 0)
+
+            
