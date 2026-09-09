b814d446dec041930012ff8879b71b5f6655b792
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-09 22:11:08 +0530

    Time: 1822 ms (12.26%), Space: 24.7 MB (6.6%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,12 +1,18 @@
-from bisect import bisect_left
-
+from functools import lru_cache
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        tails = []
-        for num in nums:
-            idx = bisect_left(tails, num)
-            if idx == len(tails):
-                tails.append(num)
-            else:
-                tails[idx] = num
-        return len(tails)
+    def lengthOfLIS(self, arr: List[int]) -> int:
+        @lru_cache(None)
+        def solve(i):
+            if i == len(arr):
+                return 0
+            ans = 1
+            for j in range(i+1, len(arr)):
+                if arr[j] <= arr[i]:
+                    continue
+                c = solve(j)
+                ans = max(ans,1+c)
+            return ans
+        ans = 0 
+        for i in range(len(arr)): 
+            ans = max(ans,solve(i))
+        return ans
