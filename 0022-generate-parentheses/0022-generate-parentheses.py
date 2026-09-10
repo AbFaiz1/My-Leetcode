1b3d05597dd45297d07fa87b490436692598a5e6
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-10 19:52:02 +0530

    Time: 0 ms (100%), Space: 19.6 MB (9.75%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,19 +1,18 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        k = 2*n
         ans = []
-        ob = n
-        cb = n
-        end = n * 2
-        def solve(i, temp, ob, cb):
-            if i == end:
-                ans.append(temp)
+        def solve(i, ob, cb, temp):
+            if i >= k:
+                ans.append("".join(temp))
                 return
-            if cb > ob:  
-                solve(i + 1, temp + ")", ob, cb - 1)
-                if ob > 0:  
-                    solve(i + 1, temp + "(", ob - 1, cb)
-            else:
-                if ob > 0:  
-                    solve(i + 1, temp + "(", ob - 1, cb)
-        solve(0, "", n, n)
+            if cb > ob:
+                temp.append(')')
+                solve(i+1, ob, cb-1, temp)
+                temp.pop()
+            if ob > 0:
+                temp.append('(')
+                solve(i+1, ob-1, cb, temp)
+                temp.pop()
+        solve(0, n, n, [])
         return ans
