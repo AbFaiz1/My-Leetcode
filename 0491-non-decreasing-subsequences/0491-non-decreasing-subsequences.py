a0be8b739f412187372317de5d8b63c20f836db8
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-10 18:21:53 +0530

    Time: 32 ms (22.07%), Space: 29.4 MB (17.58%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -3,14 +3,15 @@ class Solution:
         ans = []
         def solve(i, temp):
             if len(temp) >= 2:
-                ans.append(temp.copy())
-            for j in range(i + 1, len(arr)):
+                ans.append(temp.copy())  
+                  
+            for j in range(i+1, len(arr)):
                 if arr[j] < arr[i]:
                     continue
-                temp.append(arr[j])  
+                temp.append(arr[j])
                 solve(j, temp)
                 temp.pop()
-        for i in range(len(arr)):  
+        for i in range(len(arr)):
             solve(i, [arr[i]])
         ans = list(set(map(tuple, ans)))
         return ans
