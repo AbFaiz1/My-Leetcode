c7d5ddc829179de6ed39b1c0c2360c6ba213d48c
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-10 19:37:52 +0530

    Time: 15 ms (76.9%), Space: 19.4 MB (53.89%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -6,35 +6,20 @@ class Solution:
             return False
         target = target // k
         arr.sort(reverse=True) 
-
         if arr[0] > target: 
             return False
-
         groups = [0] * k
-
         def solve(i):
-
             if i >= len(arr):
                 return True
-
             for group in range(k):
-
                 if group > 0 and groups[group] == groups[group - 1]:
                     continue  
-
                 if groups[group] + arr[i] > target:
                     continue
-
                 groups[group] += arr[i]
-
                 if solve(i + 1):
                     return True
-
-                groups[group] -= arr[i]
-
-                if groups[group] == 0:
-                    break  
-
+                groups[group] -= arr[i]         
             return False
-
         return solve(0)
