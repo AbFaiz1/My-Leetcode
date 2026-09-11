9c1aac1bc723819c9edc23fc3069f9f65a7b2acd
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-11 17:40:47 +0530

    Time: 3766 ms (53.76%), Space: 19.4 MB (58.9%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -5,25 +5,26 @@ class Solution:
         ]
         rows = len(grid)
         cols = len(grid[0])
-        def dfs(r, c, i):
-            if r < 0  or c < 0 or r >= rows or c >= cols:
+        def dfs(r, c, word, i):
+            if r < 0 or c < 0 or r >= rows or c >= cols:
                 return False
             if grid[r][c] != word[i]:
                 return False
+            if grid[r][c] == '#':
+                return False
             if i == len(word) - 1:
-                return True 
+                return True
             temp = grid[r][c]
             grid[r][c] = '#'
             for x, y in directions:
                 nr = x + r
                 nc = y + c
-                if dfs(nr, nc, i+1):
+                if dfs(nr, nc, word, i+1):
                     return True
             grid[r][c] = temp
             return False
         for i in range(rows):
             for j in range(cols):
-                if dfs(i, j, 0):
+                if dfs(i, j, word, 0):
                     return True
         return False
-
