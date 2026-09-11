1f772eb0177103cdb6b02107c72f7dfe3f7e5bc4
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-11 19:53:47 +0530

    Time: 130 ms (71.41%), Space: 21.8 MB (93.21%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -1,25 +1,16 @@
 class Solution:
     def subarraysWithKDistinct(self, arr: List[int], k: int) -> int:
-        mp = {}
-        count = 0
-        count2 = 0
-        i = 0
-        for j in range(len(arr)):
-            mp[arr[j]] = mp.get(arr[j], 0)+1
-            while len(mp) > k:
-                mp[arr[i]] -= 1
-                if mp[arr[i]] == 0:
-                    del mp[arr[i]]
-                i += 1
-            count += j - i + 1
-        mp = {}
-        i = 0
-        for j in range(len(arr)):
-            mp[arr[j]] = mp.get(arr[j], 0)+1
-            while len(mp) > k-1:
-                mp[arr[i]] -= 1
-                if mp[arr[i]] == 0:
-                    del mp[arr[i]]
-                i += 1
-            count2 += j - i + 1 
-        return count - count2
+        def atmost(k):
+            mp = {}  
+            left = 0
+            ans = 0
+            for i in range(len(arr)):
+                mp[arr[i]] = mp.get(arr[i], 0) + 1
+                while len(mp) > k:
+                    mp[arr[left]] -= 1
+                    if mp[arr[left]] == 0:
+                        del mp[arr[left]]
+                    left += 1
+                ans += i - left + 1
+            return ans
+        return atmost(k) - atmost(k - 1)
