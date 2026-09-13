ccd3ea49ea7b2ec39a371c4719fd8387f42f3d5d
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-13 23:45:01 +0530

    Time: 0 ms (100%), Space: 19.5 MB (29.14%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -4,36 +4,47 @@ class Solution:
 
         ans = []
 
-        def solve(i, temp):
+        def solve(i, temp, dot):
+            if dot == 3:
+                last = s[i:]
+                if not last:
+                    return
+                if len(last) > 1 and last[0] == '0':  
 
-            if i == len(s):
-                if len(temp) == 4:  # FIX: exactly 4 segments
-                    ans.append(".".join(temp))
-                return
+                    return
 
-            if len(temp) == 4:  # FIX: 4 segments ke baad stop
+                elif int(last) > 255:
+
+                    return
+
+                ans.append(".".join(temp + [last]))  # FIX: last segment add
+
                 return
 
             for j in range(i, i+3):
 
-                if j < len(s):  # FIX: index boundary
+                if j < len(s):  
 
                     text = s[i:j+1]
 
                     if len(text) > 1 and text[0] == '0':  
+
                         continue
 
                     num = int(text)
 
                     if num > 255:
+
                         continue
 
                     temp.append(text)
 
-                    solve(j+1, temp)  
+                    dot += 1
 
+                    solve(j + 1, temp, dot)  
+                    dot -= 1
                     temp.pop()
 
-        solve(0, [])
+        solve(0, [], 0)
 
         return ans
