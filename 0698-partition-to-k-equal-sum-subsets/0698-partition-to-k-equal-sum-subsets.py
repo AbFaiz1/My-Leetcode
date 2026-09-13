fd33b6b60b0f9e6fbc5756205e0346e4a2688b06
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-13 19:34:44 +0530

    Time: 13 ms (77.64%), Space: 19.4 MB (80.55%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,25 +1,23 @@
 class Solution:
-
     def canPartitionKSubsets(self, arr: List[int], k: int) -> bool:
         target = sum(arr)
         if target % k != 0:
             return False
         target = target // k
-        arr.sort(reverse=True) 
-        if arr[0] > target: 
-            return False
         groups = [0] * k
+        arr.sort(reverse=True)
         def solve(i):
             if i >= len(arr):
                 return True
             for group in range(k):
                 if group > 0 and groups[group] == groups[group - 1]:
-                    continue  
+                    continue
                 if groups[group] + arr[i] > target:
                     continue
                 groups[group] += arr[i]
-                if solve(i + 1):
+                if solve(i+1):
                     return True
-                groups[group] -= arr[i]         
+                groups[group] -= arr[i] 
             return False
         return solve(0)
+                
