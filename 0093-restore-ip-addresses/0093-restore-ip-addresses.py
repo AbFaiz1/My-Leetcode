4c0cf880a7324e4c83ade116b8a84bffc8133799
Abdullah Faiz <96337279+AbFaiz1@users.noreply.github.com>
2026-09-14 09:07:10 +0530

    Time: 0 ms (100%), Space: 19.5 MB (29.16%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -10,41 +10,23 @@ class Solution:
                 if not last:
                     return
                 if len(last) > 1 and last[0] == '0':  
-
                     return
-
                 elif int(last) > 255:
-
                     return
-
                 ans.append(".".join(temp + [last]))  # FIX: last segment add
-
                 return
-
             for j in range(i, i+3):
-
                 if j < len(s):  
-
                     text = s[i:j+1]
-
                     if len(text) > 1 and text[0] == '0':  
-
                         continue
-
                     num = int(text)
-
                     if num > 255:
-
                         continue
-
                     temp.append(text)
-
                     dot += 1
-
                     solve(j + 1, temp, dot)  
                     dot -= 1
                     temp.pop()
-
         solve(0, [], 0)
-
         return ans
